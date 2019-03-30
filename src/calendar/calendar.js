a44f3277cc72711d09c6293bbad32f6af092d7e1
Evgeniy <eugenychuprov85@yandex.ru>
2019-03-30 22:22:50 +0300

    add calendar

---
+++ src/calendar/calendar.js
@@ -0,0 +1,12 @@
+$( function() {
+    $( "#datepicker" ).datepicker({
+        dayNamesMin: ["SUN", "MON" , "TUE" , "WED" , "THU" , "FRI" , "SAT"],
+        firstDay:1,
+        showButtonPanel:true,
+        showOtherMonths:true,
+        altFormat:"d",
+    });
+    var currentWeekday = $.datepicker.formatDate('dd', new Date());
+    $('.calendar__header').text(currentWeekday);
+    this.$currentDay = $.datepicker.formatDate("d", new Date());
+} );
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import "./style.scss";
 import "./buttons/buttons.js"
 import "./bubble-slider/bubble-slider.js"
 import "./pie-chart/pie-chart.js"
+import "./calendar/calendar.js"
+
+require('webpack-jquery-ui/datepicker');
 
