bf2d4b4f743ec160273ff5fdaed6cdc376b93356
Evgeniy <eugenychuprov85@yandex.ru>
2019-04-02 11:23:48 +0300

    change percent

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./style.scss";
 import "./buttons/buttons.js"
 import "./bubble-slider/bubble-slider.js"
 import "./range-slider/range-slider.js"
+import "./percent/percent.js"
 import "./pie-chart/pie-chart.js"
 import "./calendar/calendar.js"
 import "./stage/stage.js"
