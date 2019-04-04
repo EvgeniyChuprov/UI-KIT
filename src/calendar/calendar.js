3c8e58105dbb23332be9f06f84db623b598bdb5b
Evgeniy <eugenychuprov85@yandex.ru>
2019-04-04 16:11:01 +0300

    added staff

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,4 +1,4 @@
-$( function() {
+$(document).ready(function() {
     $( ".datepicker" ).datepicker({
         dayNamesMin: ["SUN", "MON" , "TUE" , "WED" , "THU" , "FRI" , "SAT"],
         firstDay:1,
@@ -9,4 +9,5 @@ $( function() {
     var currentWeekday = $.datepicker.formatDate('dd', new Date());
     $('.calendar__header').text(currentWeekday);
     this.$currentDay = $.datepicker.formatDate("d", new Date());
+   
 } );
