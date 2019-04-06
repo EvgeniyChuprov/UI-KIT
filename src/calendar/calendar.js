7c0476f9eec49922bea6cb509badf4613235e561
Evgeniy <eugenychuprov85@yandex.ru>
2019-04-06 18:41:42 +0300

    repaired calendar and range-slider

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
+    var currentWeekday = $.datepicker.formatDate('dd', new Date());
     $( ".datepicker" ).datepicker({
         dayNamesMin: ["SUN", "MON" , "TUE" , "WED" , "THU" , "FRI" , "SAT"],
         firstDay:1,
         showButtonPanel:true,
         showOtherMonths:true,
-        altFormat:"d",
+        dateFormat: "d",
+        gotoCurrent : false,
+        onSelect(selectedDate) {
+            $(".calendar__header").html(selectedDate);
+        }
     });
-    var currentWeekday = $.datepicker.formatDate('dd', new Date());
     $('.calendar__header').text(currentWeekday);
-    this.$currentDay = $.datepicker.formatDate("d", new Date());
-   
+    $.datepicker._gotoToday = function(id) {
+        var target = $(id);
+        var inst = this._getInst(target[0]);
+        if (this._get(inst, 'gotoCurrent') && inst.currentDay) {
+                inst.selectedDay = inst.currentDay;
+                inst.drawMonth = inst.selectedMonth = inst.currentMonth;
+                inst.drawYear = inst.selectedYear = inst.currentYear;
+        }
+        else {
+                var date = new Date();
+                inst.selectedDay = date.getDate();
+                inst.drawMonth = inst.selectedMonth = date.getMonth();
+                inst.drawYear = inst.selectedYear = date.getFullYear();
+                // the below two lines are new
+                this._setDateDatepicker(target, date);
+                this._selectDate(id, this._getDateDatepicker(target));
+        }
+        this._notifyChange(inst);
+        this._adjustDate(target);
+    }
 } );
--- src/range-slider/range-slider.js
+++ src/range-slider/range-slider.js
@@ -1,6 +1,10 @@
- const range = document.querySelector('.range');
-function rangeChange(){
-    var val = range.value;
-    $('.range').css({'background':'-webkit-linear-gradient(left ,#4eb7a8 0%,#4eb7a8 '+val+'%,#e5e5e5 '+val+'%, #e5e5e5 100%)'});
-}
-// range.addEventListener('input',rangeChange);
+$(function() {
+    const range = document.querySelector('.range');
+    function rangeChange(){
+        var val = range.value;
+        $('.range').css({'background':'-webkit-linear-gradient(left ,#4eb7a8 0%,#4eb7a8 '+val+'%,#e5e5e5 '+val+'%, #e5e5e5 100%)'});
+    }
+    range.addEventListener('input',rangeChange);
+}); 
+
+
