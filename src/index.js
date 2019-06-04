cdeab966616726ba6ff5255a5f1322678ccb109d
Evgeniy <eugenychuprov85@yandex.ru>
2019-06-04 13:35:54 +0300

    add favicon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 import "./style.scss";
+import './favicons/favicons.js';
 import "./map/map.js"
 import "./buttons/buttons.js"
 import "./bubble-slider/bubble-slider.js"
@@ -8,6 +9,7 @@ import "./percent/percent.js"
 import "./pie-chart/pie-chart.js"
 import "./calendar/calendar.js"
 import "./stage/stage.js"
+
 
 require('webpack-jquery-ui/datepicker');
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,126 +6,139 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output:{
-        filename:'[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module:{
-        rules:[
-            {
-                test: /\.css$/,
-                loaders: ["style-loader","css-loader"]
-            },
-            {
-                test: /\.scss$/,
-                use:[
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-            {
-                test:/\.(jpg|svg|png|gif)$/,
-                use:[
-                    {
-                        loader: 'file-loader',
-                        options:{
-                           name: '/img/[name].[ext]',
-                           outputPath:'./',
-                           useRelativePath: true 
-                        }
-                    },
-                    {
-                        loader:'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 70
-                            },
-                            mozpng: {
-                                progressive: true,
-                                quality: 70
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                        pretty: true
-                }
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options:{
-                               name: 'fonts/[name].[ext]',
-                               outputPath:'./',
-                               useRelativePath: true 
-                            }
-                        }
-                    ]
-            }
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template:'./src/index.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'ui-kit.html',
-            template:'./src/ui-kit.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'recording.html',
-            template:'./src/recording.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'online.html',
-            template:'./src/online.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'staff.html',
-            template:'./src/staff.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'discount.html',
-            template:'./src/discount.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'contacts.html',
-            template:'./src/contacts.pug'
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-        }),
-        new CleanWebpackPlugin([
-            './dist/*.*'
-        ]),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery'",
-            "window.$": "jquery"
-        }),
-        new OptimizeCssAssetsPlugin({
-           assetNameRegExp: /\.css/g,
-           cssProcessor: require('cssnano'),
-           cssProcessorPluginOptions: {
-               preset: ['default', {
-                   discatdComments: {
-                       removeAll: true
-                   }
-               }]
-           },
-           canPrint:true
-        })
+      },
+      {
+        test: /\.(jpg|svg|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '/img/[name].[ext]',
+              outputPath: './',
+              useRelativePath: true
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 75
+              },
+              mozpng: {
+                progressive: true,
+                quality: 75
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
+      },
+      {
+        test: /\.(ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '/favicons/[name].[ext]',
+              useRelativePath: true
+            }
+          }
+        ]
+
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+              outputPath: './',
+              useRelativePath: true
+            }
+          }
+        ]
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'ui-kit.html',
+      template: './src/ui-kit.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'recording.html',
+      template: './src/recording.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'online.html',
+      template: './src/online.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'staff.html',
+      template: './src/staff.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'discount.html',
+      template: './src/discount.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'contacts.html',
+      template: './src/contacts.pug'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CleanWebpackPlugin([
+      './dist/*.*'
+    ]),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery'",
+      "window.$": "jquery"
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discatdComments: {
+            removeAll: true
+          }
+        }]
+      },
+      canPrint: true
+    })
+  ]
 }
