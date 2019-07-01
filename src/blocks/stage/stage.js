ae556ff62e5a27adb13cd002a74014e6e6cf176f
Evgeniy <eugenychuprov85@yandex.ru>
2019-07-01 20:05:54 +0300

    finalized stage

--- src/blocks/stage/stage.js
+++ src/blocks/stage/stage.js
@@ -1,10 +1,4 @@
-$('.steps a').click(function () {
-
-  if ($(this).parent('li').hasClass('complete')) {
-    $(this).parent('li').removeClass('complete').nextAll().removeClass('complete');
-  } else {
-    $(this).parent('li').addClass('complete').prevAll().addClass('complete');
-  }
-
-  return false;
-});
+const max = $('.stage').data('max');
+const resuilt = 100 / (max - 1) + '%';
+$('.stage__steps').css('width', resuilt)
+$('.stage__steps:first-child').css('width','0')
